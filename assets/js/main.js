62fef4e42cf19e804769d5d2c9e75565e385665e
wholdenwingate <wiholdenwingate@davidson.edu>
2023-11-10 15:03:05 -0500

    prototype 1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,50 @@
 
 /* javascript */
+$(document).ready(function () {
+  // inital 
+  let currentCityDetails;
+  $("button:first").click(async function () {
+    currentCityDetails = await getData("https://api.teleport.org/api/urban_areas/");
+    updateCityDetails();
+  });
+  $("button:last").click(async function () {
+    // next
+    currentCityDetails = await getData("https://api.teleport.org/api/urban_areas/");
+    updateCityDetails();
+  });
+  function updateCityDetails() {
+    $(".row3 h2:nth-child(1)").text(`Population: ${currentCityDetails.population}`);
+    $(".row3 h2:nth-child(2)").text(`Language: ${currentCityDetails.languages[0].language_name}`);
+    $(".row3 h2:nth-child(3)").text(`Currency: ${currentCityDetails.currencies[0].code}`);
+
+    $(".row4 h2:nth-child(1)").text(`This City is: ${currentCityDetails.name}`);
+    $(".row4 h2:nth-child(2)").text("Score: 0");
+
+    $("#cityImage").attr("src", currentCityDetails.images[0].href);
+
+    $("#cityMap").html("City Map Content");
+  }
+})
+
+async function getData(url) {
+  let city;
+  let options = { method: "GET"};
+  await fetch(url, options)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    const cities = data._links["ua:items"];
+    console.log(cities);
+    
+    if (cities && cities.length > 0) {
+      const randomIndex = Math.floor(Math.random() * cities.length);
+      city = cities[randomIndex].href;
+    } else {
+      console.error("No cities found in the response.");
+    }
+    
+  });
+  return fetch(city).then((response) => response.json);
+}
