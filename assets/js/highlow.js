992b83563f9eaafe5f8ee17d04c642e93df72c82
wholdenwingate <wiholdenwingate@davidson.edu>
2023-11-13 23:36:28 -0500

    1.2
    adjusted high low

--- assets/js/highlow.js
+++ assets/js/highlow.js
@@ -1,36 +1,54 @@
-// JS for Higher-Population
-$(document).ready(function() {
-    let cityOne;
-    let cityTwo;
+$(document).ready(function () {
+    let score = 0;
+    let highScore = 0;
 
-    async function getCitiesData(url) {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
+    async function fetchRandomCities() {
+        try {
+            const response = await fetch("https://api.teleport.org/api/urban_areas/");
+            const data = await response.json();
+            const cities = data._links["ua:item"].map(item => item.name);
+            const randomCities = getRandomCities(cities, 2);
+
+            await fetchCityDetails(randomCities[0], "city1Img");
+            await fetchCityDetails(randomCities[1], "city2Img");
+        } catch (error) {
+            console.error("Error fetching random cities:", error);
+        }
     }
-    function getRandomCity(citiesArray) {
-        const randomIndex = Math.floor(Math.random() * citiesArray.length);
-        return citiesArray[randomIndex];
+
+    async function fetchCityDetails(cityName, imgId) {
+        const citySlug = cityName.toLowerCase().replace(/\s+/g, '-');
+
+        try {
+            const response = await fetch(`https://api.teleport.org/api/urban_areas/slug:${citySlug}/details/`);
+            const data = await response.json();
+            
+            const photos = data._links;
+            
+            if (photos) {
+                
+                const image = photos[0].image.web;
+
+                $(`#${imgId}`).attr("src", image);
+
+                console.log(`${cityName} image: ${image}`);
+            } 
+
+            const population = data.population;
+
+            console.log(`${cityName} population: ${population}`);
+        } catch (error) {
+            console.error(`Error fetching details for ${cityName}:`, error);
+        }
     }
-    async function initializeGame() {
-        const citiesData  = await getCitiesData("https://api.teleport.org/api/urban_areas/");
-        const citiesArray = citiesData._links.urban_area.map(city => city.href);
 
-        cityOne = getRandomCity(citiesArray);
-        cityTwo = getRandomCity(citiesArray);
-
-        const cityOneData = await getCitiesData(cityOne);
-        const cityTwoData = await getCitiesData(cityTwo);
-
-        console.log("City 1 Population:", cityTwoData.population);
-        console.log("City 2 Population:" , cityTwoData.population);
-
-        $("nextBtn").prop("disabled", false);
+    function getRandomCities(array, count) {
+        const shuffledArray = array.sort(() => 0.5 - Math.random());
+        return shuffledArray.slice(0, count);
     }
-    $("#clickToBeginBtn").on("click", async function () {
-        await initializeGame();   
-    });
-    $("#nextBtn").on("click", async function () {
-        await initializeGame();
+
+    
+    $("#clickToBeginBtn").click(function () {
+        fetchRandomCities();
     });
 });
