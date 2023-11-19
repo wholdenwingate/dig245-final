f17b19d2178df0c90deb00835b67ab5105be56f7
wholdenwingate <wiholdenwingate@davidson.edu>
2023-11-19 17:28:28 -0500

    1.8
    Worked on the map, still doesn't work but i have the coordinates. also need to fix bug with fetching languages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,118 @@
 
 /* javascript */
+let latitude;
+let longitude;
+let initalMapView = true;
+let map;
+
 $(document).ready(function () {
   // inital 
+  map = L.map('map').setView([0, 0], 1);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap'
+  }).addTo(map);
+
   let currentCityDetails;
   let score = 0;  
-  let gameStarted = false;
+  let highScore = localStorage.getItem('highScore') || 0;
+  $("#highScore").text(`${highScore}`);
 
-  $("button:first").click(async function () {
+  let gameStarted = false;
+  let guessSubmitted = false;
+
+  $("#beginButton").click(async function () {
     currentCityDetails = await getData("https://api.teleport.org/api/urban_areas/");
     updateCityDetails(currentCityDetails);
     gameStarted = true;
-    enableButtons();
+    guessSubmitted = false;
+    enableSubmitButton();
+    score = 0;
+    document.getElementById("score").innerText = score;
   });
 
   $("#submitGuess").click(function () {
-    if (gameStarted) {
+    if (gameStarted && currentCityDetails && !guessSubmitted) {
       const userGuess = document.getElementById("userGuess").value.toLowerCase();
-      const actualCityName = currentCityDetails.name.toLowerCase();
+      const actualCityName = currentCityDetails.cityName.toLowerCase();
 
       if (userGuess === actualCityName) {
-        alert("Correct! You earned a point.");
+        alert("Correct!");
         score++;
+        disableSubmitButton();
+        enableNextButton();
+        gameStarted = false;
+        guessSubmitted = true;
+        initalMapView = false;
+
+        if (score > highScore) {
+          highScore = score;
+          localStorage.setItem('highScore', highScore); 
+          $("#highScore").text(`${highScore}`);
+        }
+        updateMapView();
+
       } else {
-        alert("Incorrect. Try again!");
+        alert("Incorrect! Press 'Begin' to restart.");
+        enableBeginButton();
+        disableNextButton();
+        gameStarted = false;
       }
       document.getElementById("score").innerText = score;
+    
     }
   });
 
-  $("button:last").click(async function () {
+  function updateMapView() {
+  if (latitude !== undefined && longitude !== undefined && guessSubmitted) {
+    map.setView([latitude, longitude], 10);
+
+    L.marker([latitude, longitude]).addTo(map)
+      .bindPopup('Guessed Location!')
+      .openPopup();
+
+    $("#map").html(`Latitude: ${latitude}, Longitude: ${longitude}`);
+  } else {
+    console.error("Latitude or longitude is undefined.");
+    }
+  }
+
+  $("#nextButton").click(async function () {
     // next
-    if (gameStarted) {
+    if (!gameStarted && guessSubmitted) {
       currentCityDetails = await getData("https://api.teleport.org/api/urban_areas/");
       updateCityDetails(currentCityDetails);
+      enableSubmitButton();
+      disableNextButton();
+      guessSubmitted = false;
+      gameStarted = true;
     }
   });
 
-  function enableButtons() {
+  function enableSubmitButton() {
     $("#submitGuess").prop("disabled", false);
-    $("button:last").prop("disabled", false);
-}
+  }
+  function disableSubmitButton() {
+    $("#submitGuess").prop("disabled", true);
+  }
+  function enableNextButton() {
+    $("#nextButton").prop("disabled", false);
+  }
+  function disableNextButton() {
+    $("#nextButton").prop("disabled", true);
+  }
+  function disableNextButton() {
+    $("#nextButton").prop("disabled", true);
+  }
+  function enableBeginButton() {
+    $("#beginButton").prop("disabled", false);
+  }
 
   function updateCityDetails(cityDetails, image) {
     const populationElement = document.querySelector("#population");
     const languageElement = document.querySelector("#language");
     const currencyElement = document.querySelector("#currency");
-    
+
     if (populationElement && languageElement && currencyElement) {
       populationElement.innerHTML = `Population: ${cityDetails.population}`;
       languageElement.innerHTML = `Language: ${cityDetails.language}`;
@@ -53,7 +120,12 @@ $(document).ready(function () {
     }
     $("#cityImage").attr("src", image);
 
-    $("#map").html("City Map Content");
+    if (latitude !== undefined && longitude !== undefined && guessSubmitted) {
+      map.setView([latitude, longitude], 10)
+      $("#map").html(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    } else {
+      console.error("Latitude or longitude is undefined.");
+    } 
   }
 });
 
@@ -68,11 +140,9 @@ async function getData(url) {
       if (citiesData && citiesData.length > 0) {
         const randomIndex = Math.floor(Math.random() * citiesData.length);
         city = citiesData[randomIndex];
-
       } else {
         console.error("No cities found in the response.");
       }
-
     });
   console.log(city)
   const citySlug = city.name.toLowerCase().replace(/\s+/g, '-');
@@ -83,20 +153,31 @@ async function getData(url) {
   const image = photos.mobile;
   document.querySelector("#cityImage").src = image
   console.log(image)
+
   let population;
   let language;
   let currency;
+  let cityName;
+  
+
   return fetch(city.href)
     .then((response) => response.json())
     .then(data => {
       console.log(data)
+      if (data.bounding_box && data.bounding_box.latlon) {
+        latitude = data.bounding_box.latlon.north;
+        longitude = data.bounding_box.latlon.east;
+        console.log(latitude, longitude)
+      }
       const cityData = data._links["ua:details"];
       console.log(cityData)
+      cityName = city.name
       return fetch(cityData.href)
         .then((response) => response.json())
     })
     .then(dataCity => {
       console.log(dataCity)
+      
       
       for (let i = 0; i < dataCity.categories.length; i++ ) {
         const category = dataCity.categories[i];
@@ -113,17 +194,11 @@ async function getData(url) {
         population,
         language,
         currency,
-      }
-
+        cityName,
+        latitude,
+        longitude,
+      };
     });
 
 }
 
-
-// map
-var map = L.map('map').setView([0, 0], 1);
-// set the tileset
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; OpenStreetMap'
-}).addTo(map);
