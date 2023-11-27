66e95e154ed55ec6e0d2539ecd6ba807fc24725e
wholdenwingate <wiholdenwingate@davidson.edu>
2023-11-27 15:52:20 -0500

    2.6
    starting autofill

--- assets/js/main.js
+++ assets/js/main.js
@@ -238,3 +238,112 @@ function updateMapView(latitude, longitude, guessSubmitted) {
     map.setView([0, 0], 1);
   }
 }
+let autoComplete;
+autoComplete.exports = {
+  autoComplete: tarekraafat/autocomplete.js
+}
+const autoCompleteJS = new autoComplete({
+//selector: "#autoComplete",
+  data: {
+    src: async() => {
+      try {
+        document
+          .getElementById("userGuess")
+          .setAttribute("placeholder","Loading..." )
+        const source = await fetch("https://api.teleport.org/api/urban_areas");
+        const data = await source.json();
+        document
+          .getElementById("userGuess")
+          .setAttribute("userGuess", autoCompleteJS.placeHolder);
+          return data;
+        } catch (error) {
+          return error;
+        }
+    },
+    keys: ["name"],
+    cache: true,
+    filter: (list) => {
+      const filteredResults = Array.from(
+        new Set(list.map((value) => value.match))
+      ).map((name) => {
+        return list.find((value) => value.match === name);
+      });
+      return filteredResults;
+    }
+  },
+  placeHolder: "Type your guess...",
+  resultsList: {
+    element: (list, data) => {
+      const info = document.createElement("p");
+      if (data.results.length > 0) {
+        info.innerHTML = `Displaying <strong>${data.results.length}</strong> out of <strong>${data.matches.length}</strong> results`;
+      } else {
+        info.innerHTML = `Found <strong>${data.matches.length}</strong> matching results for <strong>"${data.query}"</strong>`;
+    }
+    list.prepend(info);
+  },
+  resultItem: {
+    element: (item, data) => {
+      item.style = "display: flex; justify-content: space-between;";
+      item.innerHTML = `
+      <span style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
+        ${data.match}
+      </span>
+      <span style="display: flex; align-items: center; font-size: 13px; font-weight: 100; text-transform: uppercase; color: rgba(0,0,0,.2);">
+        ${data.key}
+      </span>`;
+    },
+    highlight: true
+  },
+  events: {
+    input: {
+      focus: () => {
+        if (autoCompleteJS.input.value.length) autoCompleteJS.start();
+      }
+    }
+  }
+  }
+});
+autoCompleteJS.input.addEventListener("selection", function (event) {
+  const feedback = event.detail;
+  autoCompleteJS.input.blur();
+  const selection = feedback.selection.value[feedback.selection.key];
+  // Render selected choice to selection div
+  document.querySelector(".selection").innerHTML = selection;
+  // Replace Input value with the selected value
+  autoCompleteJS.input.value = selection;
+  // Console log autoComplete data feedback
+  console.log(feedback);
+});
+
+
+// Blur/unBlur page elements
+const action = (action) => {
+  const title = document.querySelector("h1");
+  const mode = document.querySelector(".mode");
+  const selection = document.querySelector(".selection");
+  const footer = document.querySelector(".footer");
+
+  if (action === "dim") {
+    title.style.opacity = 1;
+    mode.style.opacity = 1;
+    selection.style.opacity = 1;
+  } else {
+    title.style.opacity = 0.3;
+    mode.style.opacity = 0.2;
+    selection.style.opacity = 0.1;
+  }
+};
+
+// Blur/unBlur page elements on input focus
+["focus", "blur"].forEach((eventType) => {
+  autoCompleteJS.input.addEventListener(eventType, () => {
+    // Blur page elements
+    if (eventType === "blur") {
+      action("dim");
+    } else if (eventType === "focus") {
+      // unBlur page elements
+      action("light");
+    }
+  });
+});
